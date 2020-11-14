66fc796e85dfe45f4604ae054757248dcb9191dd
Dusty McCord <Dustymccord@gmail.com>
2020-11-14 13:23:03 -0800

    add navbar and logout modal.

--- web/src/context/SelectedContext.js
+++ web/src/context/SelectedContext.js
@@ -2,19 +2,31 @@ import React, { useState } from 'react'
 
 export const SelectedVideoContext = React.createContext()
 export const SelectedUpdateContext = React.createContext()
+export const MenuContext = React.createContext()
+export const MenuUpdateContext = React.createContext()
 
 export const SelectedProvider = ({ children }) => {
   const initialState = 'https://www.youtube.com/watch?v=ysz5S6PUM-U'
   const [selected, setSelected] = useState(initialState)
 
+  const [activeMenu, setActiveMenu] = useState('home')
+
+  const handleUpdateMenu = (name) => {
+    setActiveMenu(name)
+  }
+
   const handelUpdate = (e) => {
-    console.log('update', e.target.name)
     setSelected(e.target.name)
   }
+
   return (
     <SelectedVideoContext.Provider value={selected}>
       <SelectedUpdateContext.Provider value={handelUpdate}>
-        {children}
+        <MenuContext.Provider value={activeMenu}>
+          <MenuUpdateContext.Provider value={handleUpdateMenu}>
+            {children}
+          </MenuUpdateContext.Provider>
+        </MenuContext.Provider>
       </SelectedUpdateContext.Provider>
     </SelectedVideoContext.Provider>
   )
--- web/src/layouts/StandardLayout/StandardLayout.js
+++ web/src/layouts/StandardLayout/StandardLayout.js
@@ -1,30 +1,91 @@
-import { Container } from 'semantic-ui-react'
+import { useContext } from 'react'
+import { Container, Menu, Modal, Button, Header, Icon } from 'semantic-ui-react'
 import { Link, routes } from '@redwoodjs/router'
 import { useAuth } from '@redwoodjs/auth'
 
+import { MenuContext, MenuUpdateContext } from 'src/context/SelectedContext'
+
 const StandardLayout = ({ children }) => {
+  const activeItem = useContext(MenuContext)
+  const setActiveItem = useContext(MenuUpdateContext)
+  const [open, setOpen] = React.useState(false)
+
   const { logIn, isAuthenticated, logOut, currentUser } = useAuth()
+
+  const handleItemClick = (e, { name }) => {
+    console.log('name', name)
+    setActiveItem(name)
+  }
+
+  const triggerLogOutModal = () => {
+    setOpen(true)
+  }
+
   return (
     <Container>
-      <h1>
-        <Link to={routes.home()}>PDX McCord</Link>
-      </h1>
-      <ul>
-        <li>
-          <a href="#" onClick={isAuthenticated ? logOut : logIn}>
-            {isAuthenticated ? 'Log Out' : 'Log In'}
-          </a>
-        </li>
-        {isAuthenticated && (
-          <>
-            <li>Logged in as {currentUser.email} </li>
-            <li>
-              <Link to={routes.videos()}> add Videos</Link>
-            </li>
-          </>
-        )}
-      </ul>
+      {/* ////////////////// MODAL ///////////////// */}
+      <Modal
+        basic
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        size="small"
+        // trigger={<Button>Basic Modal</Button>}
+      >
+        <Header icon>
+          <Icon name="user circle" />
+          {isAuthenticated && `Log Out ${currentUser.email}?`}
+        </Header>
+        <Modal.Content>
+          <p>Are you sure you want to log out?</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button basic color="red" inverted onClick={() => setOpen(false)}>
+            <Icon name="remove" /> No
+          </Button>
+          <Button
+            color="green"
+            inverted
+            onClick={() => {
+              setOpen(false)
+              return logOut()
+            }}
+          >
+            <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
 
+      {/* /////////////// MENU  ////////// */}
+      <Menu pointing secondary>
+        <Link to={routes.home()}>
+          <Menu.Item
+            name="home"
+            active={activeItem === 'home'}
+            onClick={handleItemClick}
+          >
+            PDX McCord
+          </Menu.Item>
+        </Link>
+        {isAuthenticated && (
+          <Link to={routes.videos()}>
+            <Menu.Item
+              name="videos"
+              active={activeItem === 'videos'}
+              onClick={handleItemClick}
+            >
+              Modify Videos
+            </Menu.Item>
+          </Link>
+        )}
+        <Menu.Menu position="right">
+          <a href="#" onClick={isAuthenticated ? triggerLogOutModal : logIn}>
+            <Menu.Item name="auth">
+              {isAuthenticated ? `Log Out` : 'Log In'}
+            </Menu.Item>
+          </a>
+        </Menu.Menu>
+      </Menu>
       {children}
     </Container>
   )
